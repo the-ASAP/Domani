d801b7623b825088b733a7944d84b58234f67aeb
Dzennet <andy.biersack.2014@mail.ru>
2021-11-17 13:07:53 +0400

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 import * as $ from "jquery";
 import "../scss/style.scss";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel";
 
 const menuSvgMain = `
     <svg class="menu__icon"
@@ -30,107 +32,299 @@ const menuSvgClose = `
 `;
 
 const createYouTubeEmbedLink = (btn, container) => {
-    $(btn).each((i, el) => {
-        let link = $(el).attr("data-src"),
-            linkStart = "https://www.youtube.com/embed/",
-            linkEnd = "?rel=0&showinfo=0&autoplay=1";
-        let newLink = linkStart + link.slice(link.indexOf("=") + 1, link.length) + linkEnd;
-        $(el).on("click", function () {
-            $(this)
-                .parent(container)
-                .empty()
-                .append(
-                    `<iframe class="production__frame" src="${newLink}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
-                );
-        });
+  $(btn).each((i, el) => {
+    let link = $(el).attr("data-src"),
+      linkStart = "https://www.youtube.com/embed/",
+      linkEnd = "?rel=0&showinfo=0&autoplay=1";
+    let newLink =
+      linkStart + link.slice(link.indexOf("=") + 1, link.length) + linkEnd;
+    $(el).on("click", function () {
+      $(this)
+        .parent(container)
+        .empty()
+        .append(
+          `<iframe class="production__frame" src="${newLink}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+        );
     });
+  });
 };
 
 const toggleModal = (triggerClass, modalClass, classActive, closeClass) => {
-    $(triggerClass).on("click", () => {
-        $(modalClass).addClass(classActive);
-        $("body").css({ overflow: "hidden" });
-    });
-    $(closeClass).on("click", () => {
-        $(modalClass).removeClass(classActive);
-        $("body").css({ overflow: "auto" });
-    });
+  $(triggerClass).on("click", () => {
+    $(modalClass).addClass(classActive);
+    $("body").css({ overflow: "hidden" });
+  });
+  $(closeClass).on("click", () => {
+    $(modalClass).removeClass(classActive);
+    $("body").css({ overflow: "auto" });
+  });
 };
 
-const OutsideClick = (elem, activeClass = "active") => {
-    $(document).on("mousedown", function (e) {
-        if ($(elem).has(e.target).length === 0) {
-            $(elem).removeClass(activeClass);
-            $("body").css({ overflow: "auto" });
-        }
-    });
+const OutsideClick = (elem, activeClass = "active", attr = "") => {
+  $(document).on("mousedown", function (e) {
+    if (!$(elem).is(e.target) && $(elem).has(e.target).length === 0) {
+      $(elem).removeClass(activeClass);
+      $(elem).attr(attr, "");
+      $("body").css({ overflow: "auto" });
+    }
+  });
 };
 
 const owlGallery = (selector, params) => {
-    const arrow = `<svg width="12" height="8" viewBox="0 0 12 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M11 4.5C11.2761 4.5 11.5 4.27614 11.5 4C11.5 3.72386 11.2761 3.5 11 3.5V4.5ZM0.646447 3.64645C0.451185 3.84171 0.451185 4.15829 0.646447 4.35355L3.82843 7.53553C4.02369 7.7308 4.34027 7.7308 4.53553 7.53553C4.7308 7.34027 4.7308 7.02369 4.53553 6.82843L1.70711 4L4.53553 1.17157C4.7308 0.976311 4.7308 0.659728 4.53553 0.464466C4.34027 0.269204 4.02369 0.269204 3.82843 0.464466L0.646447 3.64645ZM11 3.5L1 3.5V4.5L11 4.5V3.5Z" fill="#39AD46"/>
-            </svg>`;
-    if (params == undefined) params = "";
-    const owl = $(selector);
-    owl.each((i, el) => {
-        $(el)
-            .addClass("owl-carousel owl-theme")
-            .owlCarousel(
-                Object.assign(params, {
-                    lazyLoad: true,
-                    smartSpeed: 1000,
-                    navText: [arrow, arrow],
-                })
-            );
-    }).trigger("refresh.owl.carousel");
+  let arrowIcon = `<svg width="6" height="10" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <mask id="path-1-inside-1_756:1969" fill="white">
+  <path fill-rule="evenodd" clip-rule="evenodd" d="M4.44005 0.286083L0.197414 4.52872C-0.0629354 4.78907 -0.0629354 5.21118 0.197414 5.47153L4.44005 9.71417C4.7004 9.97452 5.12251 9.97452 5.38286 9.71417C5.64321 9.45382 5.64321 9.03171 5.38286 8.77136L1.61163 5.00013L5.38286 1.22889C5.64321 0.968542 5.64321 0.546432 5.38286 0.286083C5.12251 0.025733 4.7004 0.025733 4.44005 0.286083Z"/>
+  </mask>
+  <path fill-rule="evenodd" clip-rule="evenodd" d="M4.44005 0.286083L0.197414 4.52872C-0.0629354 4.78907 -0.0629354 5.21118 0.197414 5.47153L4.44005 9.71417C4.7004 9.97452 5.12251 9.97452 5.38286 9.71417C5.64321 9.45382 5.64321 9.03171 5.38286 8.77136L1.61163 5.00013L5.38286 1.22889C5.64321 0.968542 5.64321 0.546432 5.38286 0.286083C5.12251 0.025733 4.7004 0.025733 4.44005 0.286083Z" fill="#212223"/>
+  <path d="M0.197414 4.52872L-0.745395 3.58591L-0.745395 3.58591L0.197414 4.52872ZM0.197414 5.47153L-0.745395 6.41434L0.197414 5.47153ZM4.44005 9.71417L3.49725 10.657L4.44005 9.71417ZM5.38286 8.77136L6.32567 7.82855L5.38286 8.77136ZM1.61163 5.00013L0.668819 5.94294L-0.273991 5.00013L0.668818 4.05732L1.61163 5.00013ZM-0.745395 3.58591L3.49724 -0.656726L5.38286 1.22889L1.14022 5.47153L-0.745395 3.58591ZM-0.745395 6.41434C-1.52644 5.63329 -1.52644 4.36696 -0.745395 3.58591L1.14022 5.47153C1.40057 5.21118 1.40057 4.78907 1.14022 4.52872L-0.745395 6.41434ZM3.49725 10.657L-0.745395 6.41434L1.14022 4.52872L5.38286 8.77136L3.49725 10.657ZM6.32567 10.657C5.54462 11.438 4.27829 11.438 3.49725 10.657L5.38286 8.77136C5.12251 8.51101 4.7004 8.51102 4.44005 8.77136L6.32567 10.657ZM6.32567 7.82855C7.10672 8.6096 7.10672 9.87593 6.32567 10.657L4.44005 8.77136C4.17971 9.03171 4.17971 9.45382 4.44005 9.71417L6.32567 7.82855ZM2.55444 4.05732L6.32567 7.82855L4.44005 9.71417L0.668819 5.94294L2.55444 4.05732ZM6.32567 2.1717L2.55444 5.94294L0.668818 4.05732L4.44005 0.286083L6.32567 2.1717ZM6.32567 -0.656727C7.10672 0.124322 7.10672 1.39065 6.32567 2.1717L4.44005 0.286083C4.1797 0.546432 4.1797 0.968542 4.44005 1.22889L6.32567 -0.656727ZM3.49724 -0.656726C4.27829 -1.43778 5.54462 -1.43778 6.32567 -0.656727L4.44005 1.22889C4.7004 1.48924 5.12251 1.48924 5.38286 1.22889L3.49724 -0.656726Z" fill="#212223" mask="url(#path-1-inside-1_756:1969)"/>
+  </svg>
+  `;
+
+  if (params == undefined) params = "";
+  const owl = $(selector);
+  owl
+    .each((i, el) => {
+      $(el)
+        .addClass("owl-carousel owl-theme")
+        .owlCarousel(
+          Object.assign(params, {
+            lazyLoad: true,
+            smartSpeed: 1000,
+            navText: [arrowIcon, arrowIcon],
+          })
+        );
+    })
+    .trigger("refresh.owl.carousel");
 };
 
 function accordion(btn, content, activeClass) {
-    $(btn).on("click", function () {
-        $(this).toggleClass(activeClass);
-        $(this).find(content).slideToggle();
-    });
+  $(btn).on("click", function () {
+    $(this).toggleClass(activeClass);
+    $(this).find(content).slideToggle();
+  });
 }
 
 function select(btn, content, activeClass, closeButton) {
-    $(btn || closeButton).on("click", function () {
-        $(btn).toggleClass(activeClass);
-        $(content).slideToggle();
-    });
-    $(document).on("mousedown", function (e) {
-        if (!$(btn).is(e.target) && $(btn).hasClass(activeClass)) {
-            $(btn).toggleClass(activeClass);
-            $(content).slideToggle();
-        }
-    });
+  $(btn || closeButton).on("click", function () {
+    $(btn).toggleClass(activeClass);
+    $(content).slideToggle();
+  });
+  $(document).on("mousedown", function (e) {
+    if (!$(btn).is(e.target) && $(btn).hasClass(activeClass)) {
+      $(btn).toggleClass(activeClass);
+      $(content).slideToggle();
+    }
+  });
 }
 
-$().ready(() => {
-    createYouTubeEmbedLink($(".production__button"), $(".production__video"));
+function openSelect() {
+  const selectSingle_title = $(".select__title");
 
-    toggleModal(".map__point", ".modal", "modal__active", ".modal__close");
-    // OutsideClick(".modal", "modal__active");
+  selectSingle_title.each(function () {
+    $(this).on("click", function () {
+      const selectSingle = $(this).closest(".select");
 
-    $(".menu__openCatalog").on("click", function () {
-        $(this).toggleClass(".menu__openCatalog_active");
-        $(".menuCatalog").slideToggle();
-        if ($(this).hasClass(".menu__openCatalog_active")) {
-            $(".menu__catalog").text("Закрыть");
-            $(".menu").addClass("menu_active");
-
-            $(".menu__icon").replaceWith(menuSvgClose);
-
-            $("body").css({ overflow: "hidden" });
-        } else {
-            $(".menu__catalog").text("Каталог");
-            $(".menu").removeClass("menu_active");
-
-            $(".menu__icon").replaceWith(menuSvgMain);
-
-            $("body").css({ overflow: "auto" });
-        }
+      if ("active" === selectSingle.attr("data-state")) {
+        selectSingle.attr("data-state", "");
+      } else {
+        selectSingle.attr("data-state", "active");
+      }
     });
 
-    accordion(".menuCatalog__accordion", ".menuCatalog__information", "activeAccordion");
-    accordion(".menuCatalog__mainAccordion", ".menuCatalog__information", "activeAccordion");
+    const selectSingle_labels = $(this)
+      .siblings(".select__content")
+      .children(".select__label");
+
+    selectSingle_labels.each(function () {
+      $(this).on("click", (e) => {
+        $(this)
+          .closest(".select__content")
+          .siblings(".select__title")
+          .text(e.target.textContent);
+        $(this).closest(".select").attr("data-state", "");
+      });
+    });
+  });
+}
+
+function initTabs(buttons, content) {
+  let tab = $(buttons);
+  let tabContent = $(content);
+
+  tab.each(function () {
+    $(this).on("click", () => {
+      const tabAttr = $(this).attr("data-category");
+      localStorage.setItem("content", "");
+
+      tab.removeClass("active");
+      tabContent.removeClass("active");
+
+      tabContent.each((index, element) => {
+        if (tabAttr === $(element).attr("id")) {
+          $(this).addClass("active");
+          $(`#${tabAttr}`).addClass("active");
+        }
+      });
+    });
+  });
+}
+
+function rememberCatalogContent(items) {
+  const products = $(items);
+
+  if (products.length) {
+    products.each((i, product) => {
+      $(product).on("click", function () {
+        let content = $(this).attr("data-content");
+
+        localStorage.setItem("content", content);
+      });
+    });
+  }
+}
+
+const fillCatalogContent = () => {
+  if (localStorage.getItem("content").length) {
+    const tabButtons = $(".tab__links");
+    const tabs = $(".tab__content");
+
+    tabButtons.removeClass("active");
+    tabs.removeClass("active");
+
+    tabs.each(function (i, tab) {
+      if (localStorage.getItem("content") === $(tab).attr("id")) {
+        $(tab).addClass("active");
+      }
+    });
+
+    tabButtons.each((i, button) => {
+      if (localStorage.getItem("content") === $(button).attr("data-category")) {
+        $(button).addClass("active");
+      }
+    });
+  }
+};
+
+const openModalCatalog = () => {
+  $(".menu__openCatalog").on("click", function () {
+    $(this).toggleClass("menu__openCatalog_active");
+    if (!$("header").hasClass("menu_white")) {
+      $("header").toggleClass("menu_black");
+    }
+    $(".menuCatalog").slideToggle();
+    if ($(this).hasClass("menu__openCatalog_active")) {
+      $(".menu__catalog").text("Закрыть");
+      $(".menu").addClass("menu_active");
+
+      $(".menu__icon").replaceWith(menuSvgClose);
+      $("body").css({ overflow: "hidden" });
+    } else {
+      $(".menu__catalog").text("Каталог");
+      $(".menu").removeClass("menu_active");
+
+      $(".menu__icon").replaceWith(menuSvgMain);
+
+      $("body").css({ overflow: "auto" });
+    }
+  });
+};
+
+function declOfNum(n, text_forms) {
+  if (n > 5 || n == 0) {
+    return text_forms[0];
+  }
+  if (n > 1 && n < 5) {
+    return text_forms[2];
+  }
+  if (n == 1) {
+    return text_forms[1];
+  }
+  return text_forms[3];
+}
+
+const countNews = () => {
+  const count = $(".news__count");
+  const post = $(".post");
+  const word = declOfNum(post.length, ["новостей", "новость", "новости"]);
+
+  if (count.length) {
+    count.text(`${post.length} ${word}`);
+  }
+};
+
+$().ready(() => {
+  createYouTubeEmbedLink($(".production__button"), $(".production__video"));
+
+  toggleModal(".map__point", ".modal", "modal__active", ".modal__close");
+  toggleModal(
+    ".menu__city",
+    ".choiceCity",
+    "choiceCity__active",
+    ".choiceCity__close"
+  );
+  OutsideClick(".modal", "modal__active");
+  OutsideClick(".select", "", "data-state");
+  // OutsideClick('.menuCatalog')
+
+  openSelect();
+  initTabs();
+
+  rememberCatalogContent(".product[data-content]");
+  rememberCatalogContent(".horTab[data-content]");
+
+  initTabs(".tab__links", ".tab__content");
+  initTabs(".horTab", ".variants__item");
+
+  fillCatalogContent();
+
+  openModalCatalog();
+
+  countNews();
+
+  accordion(
+    ".menuCatalog__accordion",
+    ".menuCatalog__information",
+    "activeAccordion"
+  );
+  accordion(
+    ".menuCatalog__mainAccordion",
+    ".menuCatalog__information",
+    "activeAccordion"
+  );
+
+  owlGallery(".gallery__carousel", {
+    nav: true,
+    navContainer: ".gallery__controls",
+    dots: false,
+    margin: 20,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 1,
+      },
+    },
+  });
+
+  owlGallery(".projects__carousel", {
+    nav: true,
+    navContainer: ".projects__controls",
+    dots: false,
+    margin: 20,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 2,
+      },
+    },
+  });
 });
--- webpack.common.js
+++ webpack.common.js
@@ -4,89 +4,97 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = {
-    target: "web",
-    context: path.resolve(__dirname, "src"),
-    entry: Object.assign(
-        {},
-        ...glob.sync("./src/js/*.js").map((jsFile) => ({
-            [jsFile.replace("./src/", "")]: `${jsFile}`.replace("./src", "."),
-        }))
-    ),
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "[name]",
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: "babel-loader",
-            },
-            {
-                test: /\.(scss|css)$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                    },
-                    {
-                        loader: "css-loader",
-                    },
-                    {
-                        loader: "postcss-loader",
-                    },
-                    {
-                        loader: "sass-loader",
-                    },
-                ],
-            },
-            {
-                test: /\.(html)$/,
-                use: "html-loader",
-            },
-            {
-                test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
-                type: "asset",
-                generator: {
-                    filename: "[path][name]-[hash][ext]",
-                },
-            },
-            {
-                test: /\.(gif|png|jpe?g|svg)$/i,
-                type: "asset",
-                generator: {
-                    filename: "[path][name]-[hash][ext]",
-                },
-            },
+  target: "web",
+  context: path.resolve(__dirname, "src"),
+  entry: Object.assign(
+    {},
+    ...glob.sync("./src/js/*.js").map((jsFile) => ({
+      [jsFile.replace("./src/", "")]: `${jsFile}`.replace("./src", "."),
+    }))
+  ),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name]",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "postcss-loader",
+          },
+          {
+            loader: "sass-loader",
+          },
         ],
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin({
-            filename: "css/style.css",
-            chunkFilename: "css/style.css",
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                {
-                    from: path.resolve(__dirname, "src/img"),
-                    to: path.resolve(__dirname, "dist/img"),
-                },
-                {
-                    from: path.resolve(__dirname, "src/fonts"),
-                    to: path.resolve(__dirname, "dist/fonts"),
-                },
-            ],
-        }),
-        ...glob.sync("./src/*.html").map(
-            (htmlFile) =>
-                new HtmlWebpackPlugin({
-                    inject: true,
-                    filename: path.basename(htmlFile),
-                    template: path.basename(htmlFile),
-                })
-        ),
+      },
+      {
+        test: /\.(html)$/,
+        use: "html-loader",
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+        type: "asset",
+        generator: {
+          filename: "[path][name]-[hash][ext]",
+        },
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        type: "asset",
+        generator: {
+          filename: "[path][name]-[hash][ext]",
+        },
+      },
     ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "css/style.css",
+      chunkFilename: "css/style.css",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src/img"),
+          to: path.resolve(__dirname, "dist/img"),
+        },
+        {
+          from: path.resolve(__dirname, "src/fonts"),
+          to: path.resolve(__dirname, "dist/fonts"),
+        },
+      ],
+    }),
+
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery",
+    }),
+
+    ...glob.sync("./src/*.html").map(
+      (htmlFile) =>
+        new HtmlWebpackPlugin({
+          inject: "body",
+          filename: path.basename(htmlFile),
+          template: path.basename(htmlFile),
+        })
+    ),
+  ],
 };
